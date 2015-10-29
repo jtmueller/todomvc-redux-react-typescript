e9ffaec16a7ec817b5f793b2c7f350f83f426ccf
Joel Mueller <joel.mueller@gmail.com>
2015-10-28 20:49:38 -0500

    Further improvement to typings.

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,5 +1,4 @@
 /// <reference path="classnames/classnames.d.ts" />
-/// <reference path="mocha/mocha.d.ts" />
 /// <reference path="react-redux/react-redux.d.ts" />
 /// <reference path="react/react.d.ts" />
 /// <reference path="redux-actions/redux-actions.d.ts" />
